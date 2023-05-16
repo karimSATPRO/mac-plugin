cd6e1bdb669e4e5476db24565da78424207a895a
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2023-05-16 14:58:42 +0100

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "##############################################"
+echo "############ KEYADDER[Bissfeed] ##############"
+echo "##############################################"
+wget -O /tmp/enigma2-plugin-extensions-keyadder_v1.0_all.ipk "https://github.com/ismail9875/keys/blob/main/enigma2-plugin-extensions-keyadder_v1.0_all.ipk?raw=true"
+opkg install  --force-overwrite /tmp/*.ipk
+echo ""
+cd ..
+sync
+echo "##############################################"
+echo "############ Installation Successful  ########"
+echo "############   Restart Enigma2 GUI... ########" 
+echo "##############################################"
+init 4
+sleep 2
+init 3
+exit 0
